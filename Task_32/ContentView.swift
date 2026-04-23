3a0848b9dba63291e534b6d6e9329bbec024e942
BT_C09_27 <mobileappdev@bths.edu>
2026-04-23 14:42:36 -0400

    line 17

--- Task_32/ContentView.swift
+++ Task_32/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("wow")
+            Text("third try")
         }
         .padding()
     }
