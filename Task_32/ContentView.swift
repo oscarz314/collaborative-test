46f86966499bfabdb6dd6701bafb381fed50ee70
BT_C09_26 <mobileappdev@bths.edu>
2026-04-23 14:14:08 -0400

    Initial Commit

---
+++ Task_32/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Task_32
+//
+//  Created by Student on 4/23/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Task_32/Task_32App.swift
@@ -0,0 +1,17 @@
+//
+//  Task_32App.swift
+//  Task_32
+//
+//  Created by Student on 4/23/26.
+//
+
+import SwiftUI
+
+@main
+struct Task_32App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
