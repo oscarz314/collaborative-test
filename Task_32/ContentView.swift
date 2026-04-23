1fa4969e2da5f0994dc0848fb6c4e01134e5e32e
BT_C09_26 <mobileappdev@bths.edu>
2026-04-23 14:39:00 -0400

    Changed to wow

--- Task_32/ContentView.swift
+++ Task_32/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("make this string into something else")
+            Text("wow")
         }
         .padding()
     }
