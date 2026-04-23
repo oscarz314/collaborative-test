0e0cf99fd4f5d8bea9794b776f7d9e404035a0c7
BT_C09_04 <mobileappdev@bths.edu>
2026-04-23 14:31:16 -0400

    Added Text to ContentView

--- Task_32/ContentView.swift
+++ Task_32/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Collab")
+            Text("Team")
         }
         .padding()
     }
